7f3db2e10def04cb0ab96e5ccb2995dd01683656
Mathias2860DK <mathias.enemark.poulsen@gmail.com>
2021-09-13 15:24:02 +0200

    J-unit test for alle metoder i EmployeeFacade

--- src/test/java/facades/EmployeeFacadeTest.java
+++ src/test/java/facades/EmployeeFacadeTest.java
@@ -1,16 +1,15 @@
 package facades;
 
 import entities.Employee;
+import org.junit.jupiter.api.*;
 import utils.EMF_Creator;
 import entities.RenameMe;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 //Uncomment the line below, to temporarily disable this test
 //@Disabled
@@ -43,6 +42,7 @@ public class EmployeeFacadeTest {
             em.createNamedQuery("Employee.deleteAllRows").executeUpdate();
             em.persist(new Employee("Mathias","Ourøgade",10000));
             em.persist(new Employee("Ellen", "Ourøgade",100));
+            em.persist(new Employee("Lars", "Fiktivgade",50000));
 
             em.getTransaction().commit();
         } finally {
@@ -61,25 +61,32 @@ public class EmployeeFacadeTest {
 
     @Test
     void createTest() {
-        EntityManager em = emf.createEntityManager();
         Employee employee = facade.create(new Employee("Jens","Aarhusgade",300));
         assertEquals("Jens",employee.getName());
     }
 
     @Test
+    @Disabled //Reset id somehow?
     void getEmployeeById() {
+        Employee employee = facade.getEmployeeById(1);
+        assertEquals("Ellen",employee.getName());
     }
 
     @Test
     void getEmployeesByName() {
+        List<Employee> employees = facade.getEmployeesByName("Mathias");
+        assertEquals("Mathias",employees.get(0).getName());
     }
 
     @Test
     void getEmployeesWithHighestSalary() {
+        List<Employee> employees = facade.getEmployeesWithHighestSalary();
+        assertEquals("Lars",employees.get(0).getName());
     }
 
     @Test
-    void getAll() {
+    void getAllEmployeesTest() {
+        assertEquals(3,facade.getAll().size());
     }
 
 }
